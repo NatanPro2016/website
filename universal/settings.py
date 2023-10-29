e5dba505fe12170613d5b3af30d0d6db72e1a4d9
NatanPro2016 <118159992+NatanPro2016@users.noreply.github.com>
2023-10-29 22:03:26 +0300

    Update settings.py

--- universal/settings.py
+++ universal/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'system.apps.SystemConfig',
-    'universal.apps.UniversalConfig'
+    'universal.apps.UniversalConfig',
+    "django_check_seo",
 
 ]
 
